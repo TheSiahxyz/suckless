e3407330a7fb81ef7f40c7c14e9436784e90e041
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-12-14 20:27:13 +0900

    modified config.h

--- dwm/config.h
+++ dwm/config.h
@@ -343,7 +343,7 @@ static const Key keys[] = {
     { MODKEY | ShiftMask,                   XK_b,               spawn,              { .v = (const char *[]){ "bookmarks", "-o", NULL } } },
     { MODKEY | ShiftMask | ControlMask,     XK_b,               spawn,              { .v = (const char *[]){ "bookmarks", "-p", NULL } } },
     { MODKEY | ControlMask,                 XK_b,               togglebar,          {0} },
-    { MODKEY | ShiftMask | ControlMask,     XK_b,               togglebar,          { .i = 1 } },
+    { ShiftMask | ControlMask,              XK_b,               togglebar,          { .i = 1 } },
     { MODKEY2 ,                             XK_b,               spawn,              { .v = (const char *[]){ "bookmarks", "-c", NULL } } },
     { MODKEY2 | ShiftMask,                  XK_b,               spawn,              { .v = (const char *[]){ "bookmarks", "-b", NULL } } },
     { MODKEY2 | ControlMask,                XK_b,               spawn,              { .v = (const char *[]){ "bookmarks", "-s", NULL } } },
