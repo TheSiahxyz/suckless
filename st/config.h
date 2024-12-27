8988f40fce919c8aa77a97b89cb53425781f28fb
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-12-27 22:09:12 +0900

    modified config.h

--- st/config.h
+++ st/config.h
@@ -909,7 +909,7 @@ static Key key[] = {
 	{ XK_Delete,        Mod1Mask|ShiftMask,                 "\033[255;4u",   0,      0 },
 	{ XK_i,             ControlMask,                        "\033[105;5u",   0,      0 },
 	{ XK_i,             Mod1Mask|ControlMask,               "\033[105;7u",   0,      0 },
-	{ XK_m,             ControlMask,                        "\033[109;5u",   0,      0 },
+	/* { XK_m,             ControlMask,                        "\033[109;5u",   0,      0 }, */
 	{ XK_m,             Mod1Mask|ControlMask,               "\033[109;7u",   0,      0 },
 	{ XK_space,         ControlMask|ShiftMask,              "\033[32;6u",    0,      0 },
 	{ XK_space,         Mod1Mask,                           "\033[32;3u",    0,      0 },
