85e0d21a8a203abc09b2efe3805c5eeceadad143
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2025-01-09 21:18:58 +0900

    modified dwmblocks/config.h

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,37 +1,37 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"", "sb-music", 0, 11},
-    /* {"⌨", "sb-kbselect", 0, 30}, */
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal (unused from 30)*/
+    {"", "sb-music", 0, 11},            // Music (far left)
     {"", "cat /tmp/recordingicon 2>/dev/null", 0, 9},
-    {"", "sb-queues", 10, 26},
-    {"", "sb-torrent", 20, 7},
-    {"", "sb-packages", 0, 8},
-    {"", "sb-news", 0, 6},
-    {"", "sb-mailbox", 180, 12},
-    {"", "sb-git", 60, 21},
-    /* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 34",	9000,	34}, */
-    /* {"",	"sb-price xmr Monero 🔒 33",			9000,	33}, */
-    /* {"",	"sb-price eth Ethereum 🍸 32",			9000,	32}, */
-    /* {"",	"sb-price btc Bitcoin 💰 31",			9000,	31}, */
-    /* {"", "sb-doppler", 0, 13}, */
-    {"", "sb-tasks", 60, 25},
-    {"", "sb-forecast", 0, 5},
+    {"", "sb-torrent", 20, 7},          // Torrent
+    {"", "sb-queues", 10, 26},          // Queues
+    {"", "sb-mailbox", 180, 12},        // Mailbox
+    {"", "sb-news", 0, 6},              // News
+    {"", "sb-git", 60, 21},             // Git
+    {"", "sb-tasks", 60, 25},           // Tasks
+    {"", "sb-packages", 0, 8},          // Packages
+    {"", "sb-forecast", 0, 5},          // Weather
     /* {"", "sb-nettraf", 1, 16}, */
-    {"", "sb-volume", 0, 10},
-    {"", "sb-bghitness", 0, 23},
-    {"", "sb-brightness", 0, 22},
-    {"", "sb-cpu", 60, 18},
-    {"", "sb-memory", 60, 14},
-    {"", "sb-disk", 10800, 20},
-    {"", "sb-battery", 5, 3},
-    /* {"",	"sb-moonphase",	18000,	17}, */
-    {"", "sb-inputs", 0, 29},
-    {"", "sb-internet", 5, 4},
-    {"", "sb-clock", 60, 1},
-    /* {"",	"sb-iplocate", 0,	27}, */
-    {"", "sb-ecrypt", 0, 19},
-    {"", "sb-help-icon", 0, 15},
+    {"", "sb-cpu", 60, 18},             // CPU
+    {"", "sb-memory", 60, 14},          // Memory
+    {"", "sb-disk", 10800, 20},         // Disk
+    /* {"⌨", "sb-kbselect", 0, 30}, */
+    {"", "sb-inputs", 0, 29},           // Inputs
+    {"", "sb-bghitness", 0, 23},        // Background Lightness
+    {"", "sb-brightness", 0, 22},       // Brightness
+    {"", "sb-internet", 5, 4},          // Internet
+    {"", "sb-volume", 0, 10},           // Volume
+    {"", "sb-battery", 5, 3},           // Battery
+    {"", "sb-clock", 60, 1},            // Clock
+    /* {"", "sb-moonphase", 18000, 17}, */
+    /* {"", "sb-price xmr-btc \"Monero to Bitcoin\" 🔒 34", 9000, 34}, */
+    /* {"", "sb-price xmr Monero 🔒 33", 9000, 33}, */
+    /* {"", "sb-price eth Ethereum 🍸 32", 9000, 32}, */
+    /* {"", "sb-price btc Bitcoin 💰 31", 9000, 31}, */
+    /* {"", "sb-doppler", 0, 13}, */
+    /* {"", "sb-iplocate", 0, 27}, */
+    {"", "sb-ecrypt", 0, 19},           // Ecrypt (rightmost)
+    {"", "sb-help-icon", 0, 15},        // Help Icon (rightmost)
 };
 
 // Sets delimiter between status commands. NULL character ('\0') means no delimiter.
