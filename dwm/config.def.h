4e167cf99635280c58f533eaeb9c2e8cce9db1e3
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2025-06-16 23:55:10 +0900

    modified dwm/config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -495,7 +495,6 @@ static Keychord *keychords[] = {
   &((Keychord){2, {{WINKEY, XK_space},{0, XK_m}},                   spawn,                  SHCMD(TERMINAL " -n ncmpcpp -e ncmpcpp") }),
   &((Keychord){2, {{WINKEY, XK_space},{0, XK_n}},                   spawn,                  SHCMD(TERMINAL " -e newsboat; pkill -RTMIN+19 dwmblocks") }),
   &((Keychord){2, {{WINKEY, XK_space},{0, XK_p}},                   spawn,                  {.v = (const char *[]){ TERMINAL, "-e", "profanity", NULL } } }),
-  &((Keychord){2, {{WINKEY, XK_space},{0, XK_r}},                   spawn,                  {.v = (const char *[]){ TERMINAL, "-e", "htop", NULL } } }),
   &((Keychord){2, {{WINKEY, XK_space},{0, XK_t}},                   spawn,                  {.v = (const char *[]){ "torwrap", NULL } } }),
   &((Keychord){3, {{WINKEY, XK_space},{0, XK_v},{0, XK_w}},         spawn,                  {.v = (const char *[]){ TERMINAL, "-e", "nvim", "-c", "VimwikiIndex", "1", NULL } } }),
   &((Keychord){2, {{WINKEY, XK_space},{0, XK_w}},                   spawn,                  SHCMD(TERMINAL " -e less -Sf ${XDG_CACHE_HOME:-${HOME}/.cache}/weatherreport") }),
