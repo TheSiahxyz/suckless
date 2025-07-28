025f6344d97a828cc4b507ddef64b9ea77805291
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2025-07-28 16:00:14 +0900

    modified st/x.c

--- st/x.c
+++ st/x.c
@@ -1596,14 +1596,18 @@ xmakeglyphfontspecs(XftGlyphFontSpec *specs, const Glyph *glyphs, int len, int x
 void
 chgalpha(const Arg *arg)
 {
-   if (arg->f == -1.0f && alpha >= 0.1f)
-      alpha -= 0.1f;
-   else if (arg->f == 1.0f && alpha < 1.0f)
-      alpha += 0.1f;
-   else if (arg->f == 0.0f)
-      alpha = alpha_def;
-   else
-      return;
+   if (arg->f == 0.0f) {
+     /* reset */
+     alpha = alpha_def;
+   } else {
+     /* relative change: clamp to [0,1] */
+     float newα = alpha + arg->f;
+     if (newα < 0.0f)
+       newα = 0.0f;
+     else if (newα > 1.0f)
+       newα = 1.0f;
+     alpha = newα;
+   }
 
    dc.col[defaultbg].color.alpha = (unsigned short)(0xFFFF * alpha);
    /* Required to remove artifacting from borderpx */
