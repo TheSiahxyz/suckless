286ed56140986b140f76d2270434441c4caabc07
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2026-06-10 14:48:15 +0900

    modified st/x.c

--- st/x.c
+++ st/x.c
@@ -384,7 +384,11 @@ void
 zoomreset(const Arg *arg)
 {
 	Arg larg;
-	zoomabs(&larg);
+
+	if (defaultfontsize > 0) {
+		larg.f = defaultfontsize;
+		zoomabs(&larg);
+	}
 }
 
 void
@@ -1113,7 +1117,6 @@ xloadfonts(const char *fontstr, double fontsize)
 		die("can't open font %s\n", fontstr);
 
 	if (fontsize > 1) {
-		fontsize = roundf(fontsize); /* snap DPI/zoom sizes to integer px for crisp rendering */
 		FcPatternDel(pattern, FC_PIXEL_SIZE);
 		FcPatternDel(pattern, FC_SIZE);
 		FcPatternAddDouble(pattern, FC_PIXEL_SIZE, (double)fontsize);
