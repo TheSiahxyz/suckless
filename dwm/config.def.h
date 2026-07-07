62f99e5efc251099802c294118c69e3d28c96a8f
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2026-07-07 11:12:43 +0900

    modified dwm/config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -150,7 +150,7 @@ static const Rule rules[] = {
    */
   /* class                instance            title           tags mask  allowkill  focusopacity    unfocusopacity    isfloating  isterminal  noswallow  monitor  resizehints  border width */
   { TERMCLASS,            TERMINAL,           TERMINAL,       1 << 0,     1,        activeopacity,  inactiveopacity,  0,          1,          0,        -1,       1,           -1 },
-  { "zeditor",            "zeditor",          NULL,           1 << 0,     1,        activeopacity,  inactiveopacity,  0,          0,          0,         1,       1,           -1 },
+  { "dev.zed.Zed",        "dev.zed.Zed",      NULL,           1 << 0,     1,        activeopacity,  inactiveopacity,  0,          0,          0,         1,       1,           -1 },
   { BROWSER,              "Navigator",        NULL,           1 << 1,     1,        activeopacity,  inactiveopacity,  0,          0,         -1,         0,       1,           -1 },
   { BROWSER2,             "Navigator",        NULL,           1 << 1,     1,        activeopacity,  inactiveopacity,  0,          0,         -1,         1,       1,           -1 },
   { NULL,                 "libreoffice",      NULL,           1 << 2,     1,        activeopacity,  inactiveopacity,  0,          0,          0,         0,       1,           -1 },
